6754d7a73ea0b96c383080a643bd2cbd34895180
cmtran09 <cmtran@hotmail.co.uk>
2020-05-15 19:08:08 +0100

    basic react client running

--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -1,17 +1,12 @@
 import React from 'react'
 import '../../styles/styles.scss'
 
-
 export default function Chat() {
   console.log("hi")
   return (
     <div>
-      <p className='cmtran09head'>
-        Project Z - Socket.io - chatapp
-        </p>
-
       <div className='main-app'>
-        hello world
+        hello world caht compneont
         </div>
     </div>
   )
