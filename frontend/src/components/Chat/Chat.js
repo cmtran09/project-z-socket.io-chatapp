5d850ac8f82f3f1e2252f80056a8a38476a3baa3
cmtran09 <cmtran@hotmail.co.uk>
2020-05-21 14:11:35 +0100

    current useres in chatroom added and working. updates when enter and leaving

--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -8,7 +8,7 @@ let socket
 export default function Chat(props) {
 
   const [userMsg, setUserMsg] = useState('')
-  const [allMsg, setAllMsg] = useState([{ username: 'tom', message: 'hwllo' }, { username: 'tom', message: 'my' }, { username: 'tom', message: 'name' }, { username: 'tom', message: 'is' }, { username: 'tom', message: 'a' }, { username: 'tom', message: 'test' }])
+  const [allMsg, setAllMsg] = useState([{ username: 'tom', message: 'hello' }, { username: 'tom', message: 'my' }, { username: 'tom', message: 'name' }, { username: 'tom', message: 'is' }, { username: 'tom', message: 'a' }, { username: 'tom', message: 'test' }])
   // const [userName, setUserName] = useState('')
 
   const [allInRoom, setAllInRoom] = useState('')
@@ -30,40 +30,35 @@ export default function Chat(props) {
     })
 
     // provides disconnect when unmounting the component
-    return () => {
-      socket.emit('disconnect')
-      socket.off()
-    }
-  }, [])
+    // return () => {
+    //   socket.emit('disconnect')
+    //   socket.off()
+    // }
+  }, [props.location])
 
   useEffect(() => {
     socket.on('msg', (userMsg) => {
-      setAllMsg([...allMsg, userMsg])
+      setAllMsg(allMsg => [...allMsg, userMsg])
     })
-  }, [allMsg])
 
-  useEffect(() => {
     socket.on('getAllUsers', ({ users }) => {
       console.log('getalluseres useeffect')
       setAllInRoom(users)
     })
-  }, [allInRoom])
+  }, [])
 
-  //a functiont to send a user Message using the 'sendMsg' event on the back
+  //a function to send a user Message using the 'sendMsg' event on the back
   const sendMsg = (e) => {
     e.preventDefault()
     if (userMsg) {
-      console.log('userMsg', userMsg)
       socket.emit('sendMsg', { message: userMsg, room }, () => setUserMsg(''))
     }
   }
 
-  console.log("userMsg", userMsg, 'allMsg', allMsg)
-
   return (
     <div>
       <div className='main-app'>
-        hello world caht compneont
+        hello world chat component
       </div>
       {allMsg.map((elem, i) => <p key={i}>{`${elem.message}: by ${elem.username}`}</p>)}
       <input
@@ -71,11 +66,11 @@ export default function Chat(props) {
         onKeyPress={e => e.key === 'Enter' ? sendMsg(e) : null}
       />
       <button onClick={e => sendMsg(e)}>send</button>
-      <button onClick={e => console.log(userMsg)}>userMsg</button>
-      <button onClick={e => console.log(allMsg)}>allMsg</button>
-      <button onClick={e => console.log(allInRoom)}>allInRoom</button>
-      <button onClick={e => console.log(props.location.props.username)}>props</button>
-      <h1>useres</h1>
+      <button onClick={() => console.log(userMsg)}>userMsg</button>
+      <button onClick={() => console.log(allMsg)}>allMsg</button>
+      <button onClick={() => console.log(allInRoom)}>allInRoom</button>
+      <button onClick={() => console.log(props.location.props.username)}>props</button>
+      <h1>users</h1>
       {allInRoom &&
         allInRoom.map((elem, i) => {
           return (
